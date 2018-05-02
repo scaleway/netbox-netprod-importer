6ce7be5ce12111ce45df1c0b5368b260fdd1ef21
Anthony Ruhier <aruhier@online.net>
2018-05-02 15:53:12 +0200

    Add test for aggreg with multiple ports

--- tests/test_parser_nxos.py
+++ tests/test_parser_nxos.py
@@ -33,20 +33,16 @@ class TestNXOSParser():
 
         assert port_channels == expected_port_channels
 
-    @pytest.mark.skip(reason="mocks not yet implemented")
     def test_group_interfaces_by_aggreg_multiple_netif(self):
-        interfaces =  sorted((
-            "mgmt0", "Ethernet1/1", "Ethernet1/2", "Ethernet1/3",
-            "port-channel10", "port-channel11", "port-channel12", "Vlan1",
-            "Vlan200",
-        ))
+        interfaces =  [
+            "Ethernet1/1", "Ethernet1/2", "Ethernet1/3", "mgmt0",
+            "port-channel10", "port-channel11", "port-channel12", "Vlan200",
+        ]
 
         expected_port_channels = {
             "port-channel10": ["Ethernet1/1", "Ethernet1/3"],
             "port-channel12": ["Ethernet1/2"],
         }
-
-        interfaces = sorted(("Ethernet1/1", ))
 
         port_channels = self.parser.group_interfaces_by_aggreg(interfaces)
 
