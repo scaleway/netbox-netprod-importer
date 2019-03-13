0804c624c7a74b160b83345c60455bc92170a64f
Victor Pavlushin <victor@pavlushin.info>
2019-03-13 16:31:25 +0700

    1) Change parse show interface status, if Type "1000BaseSX SFP", only SFP was taken 2) Changed regex, not all types were defined 3) Added auto tests for parsing

---
+++ tests/test_parser_ios.py
@@ -0,0 +1,42 @@
+import os
+import napalm
+import pytest
+
+from netbox_netprod_importer.vendors.cisco import IOSParser
+
+BASE_PATH = os.path.dirname(__file__)
+
+
+class TestIOSParser():
+    device = None
+
+    @pytest.fixture(autouse=True)
+    def build_device(self, monkeypatch):
+        driver = napalm.get_network_driver("mock")
+
+        optional_args = {
+            "path": os.path.join(BASE_PATH, "mock_driver/specific/cisco/ios"),
+            "profile": ["ios"],
+        }
+        self.device = driver(
+            "localhost", "foo", "bar", optional_args=optional_args
+        )
+        self.device.open()
+        self.parser = IOSParser(self.device)
+
+
+    def test_get_interface_type(self):
+        assert '1000BASE-T (1GE)' == self.parser.get_interface_type(
+            "GigabitEthernet1/0/24")
+        assert 'SFP (1GE)' == self.parser.get_interface_type(
+            "GigabitEthernet1/0/28"
+        )
+        assert 'SFP+ (10GE)' == self.parser.get_interface_type(
+            "TenGigabitEthernet1/1/1"
+        )
+        assert 'SFP+ (10GE)' == self.parser.get_interface_type(
+            "TenGigabitEthernet1/1/6"
+        )
+        assert 'SFP+ (10GE)' == self.parser.get_interface_type(
+            "TenGigabitEthernet1/2/6"
+        )
