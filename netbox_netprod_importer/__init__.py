cb0a7ef2f977f34973eddf397a1794ad7bb1fe8d
Anthony Ruhier <anthony.ruhier@gmail.com>
2019-07-27 23:37:40 +0200

    Bump version: 0.1.1 → 0.2.0

--- netbox_netprod_importer/__init__.py
+++ netbox_netprod_importer/__init__.py
@@ -1,4 +1,4 @@
 __author__ = """Scaleway"""
 __email__ = "contact@scaleway.com"
-__version__ = "0.1.1"
+__version__ = "0.2.0"
 __appname__ = "netbox-netprod-importer"
--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://gitlab.infra.online.net/network/netbox_netprod_importer",
-    version="0.1.1",
+    version="0.2.0",
     zip_safe=False,
     entry_points={
         'console_scripts': [
