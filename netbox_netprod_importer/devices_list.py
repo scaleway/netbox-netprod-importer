ee61cd106cf6f0f91b34e890a8bd3a4ae7ba0cbc
Victor Pavlushin <victor@pavlushin.info>
2019-10-11 07:51:33 +0700

    remove items

--- netbox_netprod_importer/devices_list.py
+++ netbox_netprod_importer/devices_list.py
@@ -51,7 +51,7 @@ def parse_filter_yaml_def(filter_yaml, creds=None):
             raise Exception("Not for one platform napalm_driver is not "
                             "defined")
 
-        devlist = netbox_api.get("dcim/devices/", params=yml["filter"].items())
+        devlist = netbox_api.get("dcim/devices/", params=yml["filter"])
         for device in devlist["results"]:
             if not device.get("platform") or \
                     not platforms.get(device["platform"]["id"]):
